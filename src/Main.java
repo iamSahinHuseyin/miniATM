7d0237e39a43794a866d9b92ff67cc84bc0069b6
sahin <sahinhuseyinn0307@gmal.com>
2024-05-19 21:40:05 +0300

    if commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+    //while döngüsü yardımıyla mini ATM uygulaması
+
+        String sysTcNo = "999999999";
+        String sysSifre = "030799";
+        String tcNo, sifre;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Lütfen TC Kimlik numaranızı giriniz:");
+        tcNo = sc.nextLine();
+        System.out.print("Lütfen şifrenizi giriniz:");,
+        sifre = sc.nextLine();
+
+        if(tcNo.length() == 11) {
+            if (sysTcNo==tcNo || sifre==sifre) {
+                System.out.print("Başarı bir şekilde giriş yaptınız.");
+            }
+        }
+        else{
+            System.out.print("Tc Kimlik numaranız 11 haneli olmalıdır!");
+        }
+
+
 
     }
 }
